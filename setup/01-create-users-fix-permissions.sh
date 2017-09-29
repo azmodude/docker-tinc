b6f54cd0ca6aac6638e311acd3e98117a5c724cb
Gordon Schulz <gordon.schulz@gmail.com>
2017-09-29 18:35:19 +0200

    Fix formatting

--- setup/01-create-users-fix-permissions.sh
+++ setup/01-create-users-fix-permissions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv /bin/bash
 
->&2 echo "Adding ${RUN_USER} with UID ${RUN_UID} and Group ${RUN_GROUP} with GID ${RUN_GID}."
+>&2 echo "Adding '${RUN_USER}' with UID ${RUN_UID} and group '${RUN_GROUP}' with GID ${RUN_GID}."
 groupadd -g "${RUN_GID}" "${RUN_GROUP}" || true && \
     useradd -c 'Run User' -s /bin/bash -m -g "${RUN_GID}" \
     -u "${RUN_UID}" "${RUN_USER}" || true
