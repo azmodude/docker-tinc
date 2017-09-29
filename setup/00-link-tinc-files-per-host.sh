ea23f49bb35c823c5422d56e324a0f22d1b6ea09
Gordon Schulz <gordon.schulz@gmail.com>
2017-09-29 09:40:26 +0200

    Clarify error condition if TINC_HOSTNAME not set

--- setup/00-link-tinc-files-per-host.sh
+++ setup/00-link-tinc-files-per-host.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/with-contenv /bin/bash
 set -e
 
+[[ -z "${TINC_HOSTNAME}" ]] && \
+    echo "[$(basename $0)] TINC_HOSTNAME not defined." && \
+    echo "[$(basename $0)] tinc will fail to startup as configuration files are missing." && \
+    exit 1
+
 [ -e /etc/tinc/"${TINC_NETWORK}" ] || mkdir -p /etc/tinc/"${TINC_NETWORK}"
 find /etc/tinc/"${TINC_NETWORK}"-config/configs/"${TINC_HOSTNAME}" -mindepth 1 -maxdepth 3 | while read -r filename; do
     destination=/etc/tinc/"${TINC_NETWORK}"/$(basename "${filename}")
