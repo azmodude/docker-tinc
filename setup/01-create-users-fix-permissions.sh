47f397bea287616c5bfad5d54b02d91b48146cdd
Gordon Schulz <gordon.schulz@gmail.com>
2018-01-03 14:25:39 +0100

    Fix Maildir creation issue

--- setup/01-create-users-fix-permissions.sh
+++ setup/01-create-users-fix-permissions.sh
@@ -2,7 +2,7 @@
 
 >&2 echo "adding '${RUN_USER}' with UID ${RUN_UID} and group '${RUN_GROUP}' with GID ${RUN_GID}"
 groupadd -g "${RUN_GID}" "${RUN_GROUP}" || true && \
-    useradd -c 'Run User' -s /bin/bash -m -g "${RUN_GID}" \
+    useradd -K MAIL_DIR=/tmp -c 'Run User' -s /bin/bash -m -g "${RUN_GID}" \
     -u "${RUN_UID}" "${RUN_USER}" || true
 >&2 echo "fixing and setting permissions"
 chown -R "${RUN_USER}":"${RUN_GROUP}" /etc/tinc/"${TINC_NETWORK}"
